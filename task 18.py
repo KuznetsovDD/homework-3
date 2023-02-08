31cbba85c72fbc6c93353ac29106df19c249a5a6
Atlant <cuz.den30@gmail.com>
2023-02-08 23:54:43 +0300

    task 20

--- task 18.py
+++ task 18.py
@@ -9,3 +9,30 @@
 # 1 2 3 4 5
 # 6
 # -> 5
+
+# col = int(input('Введите длинну массива: '))
+
+# array = list()
+# from random import randint 
+# for _ in range(col):
+#     array.append(randint(0, col))
+
+# print(array)
+
+# numb = int(input('Введите число которое необходимо найти: '))
+
+# ar= sorted(array)
+# print(ar)
+# a=list()
+# for i in range(len(ar)):
+#     res = ar[i]-numb
+#     a.append(res)
+# sorted(a)
+# print(a)
+    
+l = [5, 78, 45, 12, 56, 9999]
+
+def nearest(lst, target):
+  return min(lst, key=lambda x: abs(x-target))
+
+print(nearest(l, 2))
